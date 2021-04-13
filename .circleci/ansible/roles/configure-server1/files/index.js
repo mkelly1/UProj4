faaf30d00fad19bd367eb53ba345e1ad261dfcda
mkelly1 <margokelly8888@gmail.com>
2021-04-13 17:03:29 -0400

    changed bucketname to udapeopleIDnum. added smoke test. added WorkflowID to frontend.yml output

--- .circleci/ansible/roles/configure-server1/files/index.js
+++ .circleci/ansible/roles/configure-server1/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
 res.writeHead(200);
-res.end("Hello world!");
+res.end("Welcome to Udapeople JS");
 });
-server.listen(3000);
+server.listen(3030);
